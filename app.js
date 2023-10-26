f233f1457d913004e33e55e39c49608d31b44206
choudhary98akash@gmail.com <akash.choudhary@biz2credit.com>
2023-10-26 10:19:17 +0530

    this commit at 10-18 and better one

--- app.js
+++ app.js
@@ -1,36 +1,9 @@
 const express = require('express')
 const app = express();
 const fs = require('fs');
-
-// here we are importing the data from the data.json file or we can use database otherwise too
 const data = require('./data');
 const { receiveMessageOnPort } = require('worker_threads');
 const { error } = require('console');
-
-const sampleData = []
-const customData = { name :'Akash ' , age: 23};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // get method to for homepage
@@ -38,7 +11,6 @@ app.get('/',(req,res)=>{
     // console.log(data)
     res.status(200).send("Rest Apis")
 })
-
 
 
 
@@ -51,11 +23,11 @@ app.get('/users',(req,res)=>{
 // get method to fetch data through request parameters
 app.get('/users/:id',(req,res)=>{
    const {id} = req.params
-   const response = data;
-   if(id>=0 && id<=data.length)
-      res.status(200).send(response.find(item=>item.id === Number(id)))
-   else
-      res.status(200).send("user dosn't exists")
+   const index = data.findIndex(item=>item.id ===Number(id))
+
+   if(index ===-1)
+        res.status(200).send("Users doesn't Exists")
+   res.status(200).send(data[index])
 
 })
 
@@ -63,7 +35,7 @@ app.get('/users/:id',(req,res)=>{
 
 
 // Post method to insert dummy data 
-app.post('/add',(req,res)=>{
+app.post('/users/add',(req,res)=>{
 
 
     // dummy data 
@@ -92,12 +64,12 @@ app.post('/add',(req,res)=>{
 
 
 // put request to modify the data
-app.put('/modify/:uid', (req, res) => { 
+app.put('/users/:uid', (req, res) => { 
 
     // New data to be manipulated 
     const userID = Number(req.params.uid)  
-    const name ='Bilal Saeed';
-    const address = 'Shaheen bagh';
+    const name ='help';
+    const address = 'Nope';
     
     
     
@@ -131,14 +103,14 @@ app.put('/modify/:uid', (req, res) => {
 
 
 // patch request to modify the data
-app.patch('/modify/:uName', (req, res) => { 
+app.patch('/users/:uName', (req, res) => { 
 
     const {uName} =req.params  
     const ind = data.findIndex(item =>item.name===uName)
     if(ind==-1)
         res.send('User not found').status(200)
     
-    data[ind].name= "Sanatan hi satya hai"  // here input the datta to be updated
+    data[ind].name= "Karan"  // here input the datta to be updated
 
 
     // write the data to update the json in file
@@ -154,7 +126,7 @@ app.patch('/modify/:uName', (req, res) => {
 
   })
 
-app.delete('/delete/:id',(req,res)=>{
+app.delete('/users/:id',(req,res)=>{
     const {id} = req.params
     const index = data.findIndex(item=>item.id ===Number(id))
     if(index === -1)
