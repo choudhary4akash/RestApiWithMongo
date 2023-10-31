9310a1b59a0eaa4f662daf0c0d6b5ffe5b6d4a05
choudhary98akash@gmail.com <akash.choudhary@biz2credit.com>
2023-10-31 10:27:23 +0530

    REst api with Mongodb

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
 const express = require('express')
 const app = express();
-const fs = require('fs');
-const data = require('./data');
 const { receiveMessageOnPort } = require('worker_threads');
 const { error } = require('console');
+const mongoose = require('mongoose')
+// const {userModel} = require('./models/addUser')
+
+
+
+// Establishing a db connection to mydb -> database
+mongoose.connect('mongodb://localhost:27017/mydb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+// Eror handling in case of error connection
+const db =  mongoose.connection
+db.on('error',(error)=>{
+    console.log(`Mongdb Connection Error`)
+})
+db.on('open',()=>{
+    console.log(`Connected to Mongodb`)
+})
+
+
+const userSchema = new mongoose.Schema({
+     id: Number,
+     name:String,
+     address:String,
+   
+  });
+const userModel = mongoose.model('users', userSchema);
+
+
+
+
+
+app.use((req,res,err,next)=>{
+    console.log(err)
+    res.status(500).send('Something went wrong.')
+    next();
+})
 
 
 // get method to for homepage
@@ -15,19 +51,36 @@ app.get('/',(req,res)=>{
 
 
 // get method to fetch the data
-app.get('/users',(req,res)=>{
-    res.status(200).send(data)
+app.get('/users',async(req,res)=>{
+
+    try {
+        const users = await userModel.find({})
+        res.status(200).send(users)
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Could not fetch')
+    }
+
+   
 })
 
 
 // get method to fetch data through request parameters
-app.get('/users/:id',(req,res)=>{
+app.get('/users/:id',async(req,res)=>{
    const {id} = req.params
-   const index = data.findIndex(item=>item.id ===Number(id))
 
-   if(index ===-1)
-        res.status(200).send("Users doesn't Exists")
-   res.status(200).send(data[index])
+
+   try {
+       const user = await userModel.find({id:Number(id)})
+    
+       if(user.length === 0)
+            res.status(200).send("Users doesn't Exists")
+       else
+            res.status(200).send(user)
+    
+   } catch (error) {
+        console.log("Error occured : "+error)
+   }
 
 })
 
@@ -35,123 +88,90 @@ app.get('/users/:id',(req,res)=>{
 
 
 // Post method to insert dummy data 
-app.post('/users/add',(req,res)=>{
+app.post('/users/add',async(req,res)=>{
 
-
-    // dummy data 
-    const uid = Number(data.length+1);
-    const name = 'Rehnuma';
-    const add = 'Faridabad'
-
-
-
-    const newData = data.push({id:uid,name:name,address:add})  // pushing data to array 
-
-
-    // writing the modified js object to file
-    fs.writeFile('./data.json',JSON.stringify(data),'utf-8',(err)=>{
-        if(err){
-            console.log(err)
-            res.status(500).send('Operation failed')
-        }
-        else{
-            console.log('Post  Succesfull')
-            res.status(201).send(data.find(item=>item.id === uid))}
-    })
     
+    try {
+        const user = new userModel({id:111,name:'NewName',address:"Riana Aurila"})
+        await user.save()
+        res.send(user).status(201)
+        
+    } catch (error) {
+        console.log("Error occured ->"+error)   
+        res.send('Operation Failed').status(500)
+    }
 
 })
 
 
+
+
+
+
+
 // put request to modify the data
-app.put('/users/:uid', (req, res) => { 
+app.put('/users/:uid', async(req, res) => { 
 
-    // New data to be manipulated 
-    const userID = Number(req.params.uid)  
-    const name ='help';
-    const address = 'Nope';
-    
-    
-    
-    const ind = data.findIndex(item =>item.id===Number(userID))
-    if(ind===-1)
-        res.status(200).send('User not found')
-
-
-    data[ind] = {id:userID,name:name,address:address};   //updating the data
-
-    res.status(200).send(`Modoified ->${JSON.stringify(data[ind])}`)   
-
-
-
-// writing the data to file for update
-    fs.writeFile('./data.json',JSON.stringify(data),'utf-8',(err)=>{
-        if(err){
-            console.log(err)
-            res.status(500).send('Operation failed')
+    const id = Number(req.params.uid)
+    try {
+        const op = await userModel.findOneAndReplace({id:id},{name:'net',id:id,address:'Whole item has been changed via put'},{new:true})
+        if (op ===null) {
+            res.status(200).send('User does not  exists for put operation')
+        } else {
+            res.status(200).send('Put performed with : '+op)
         }
-        else{
-            console.log('Modification  Succesfull')
-            res.status(201).send(data.find(item=>item.id === uid))}
-    })
+    
+    } catch (error) {
+        console.log(error)
+        res.status(500).send("Operation failed")
+    }
 
+    
   })
-
 
 
 
 
 
 // patch request to modify the data
-app.patch('/users/:uName', (req, res) => { 
+app.patch('/users/:uid', async(req, res) => { 
 
-    const {uName} =req.params  
-    const ind = data.findIndex(item =>item.name===uName)
-    if(ind==-1)
-        res.send('User not found').status(200)
+    const {uid} =req.params  
     
-    data[ind].name= "Karan"  // here input the datta to be updated
+    try {
+        const updateUser = await userModel.findOneAndUpdate({id:uid},{address:'Address has been patched.',name :"farq hai"},{new:true})
+        if(updateUser!=null)
+            res.status(201).send(updateUser)
+        else
+            res.status(200).send('User not found')
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('patch failed')
+    }
+    
 
-
-    // write the data to update the json in file
-    fs.writeFile('./data.json',JSON.stringify(data),'utf-8',(err)=>{
-        if(err){
-            console.log(err)
-            res.status(500).send('Operation failed')
-        }
-        else{
-            console.log('Patch  Succesfull')
-            res.status(201).send(JSON.stringify(data[ind]))}
-    })
-
-  })
-
-app.delete('/users/:id',(req,res)=>{
-    const {id} = req.params
-    const index = data.findIndex(item=>item.id ===Number(id))
-    if(index === -1)
-        res.status(200).send("User not found")
-
-    data.splice(index,1);
-
-    // write the data to update the json in file
-    fs.writeFile('./data.json',JSON.stringify(data),'utf-8',(err)=>{
-        if(err){
-            console.log(err)
-            res.status(500).send('Operation failed')
-        }
-        else{
-            console.log('Delete  Succesfull')
-            res.status(201).send('Deleted')}
-    })
 })
 
 
 
-// using post with query string to add data 
-// design this if u got time
 
+app.delete('/users/:id',async(req,res)=>{
+    const {id} = req.params
 
+    
+    try {
+        const deleteUser = await userModel.findOneAndDelete({id:Number(id)})
+        if(deleteUser!=null)
+            res.status(200).send("Deleted")
+        else
+            res.status(200).send('User not found')
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Operation failed')
+    }
+    
+})
 
 // used basically for cross origin resource sharing
 app.options('/users', (req, res) => {
@@ -165,3 +185,5 @@ app.options('/users', (req, res) => {
 app.listen(5000,()=>{
     console.log('Server is running at http://127.0.0.1:5000')
 })
+
+
